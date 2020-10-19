af40906e36714e516c017965dec1d7f8d79df2ab
Erikvoragate <54536359+Erikvoragate@users.noreply.github.com>
2020-10-19 11:18:21 +0200

    uppgifter

---
+++ kap2/uppgigift-2-2.php
@@ -0,0 +1,2 @@
+/**
+Använd formuläret från uppgift 1.2. Skapa ett skript som tar emot data från detta formulär: Skriptet ska skriva ut "Namn:" följt av namnet på personen, "epostadress:" och personens epostadress och till sist "Vi kommer att kontakta dig inom snarast per " följt av antingen epost eller telefon beroende på vad användaren valt. */'
